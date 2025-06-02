b0ad9730c9ac875be894cf66383742059e8b799e
Yassine Mazyane <mazyane.yassinee@gmail.com>
2025-06-02 17:09:14 +0100

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,3 @@
-const Fist = "C is fun";
-const second = "Python is cool";
-const third = "Javascript is amazing";
-
-console.log(Fist);
-console.log(second);
-console.log(third);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
