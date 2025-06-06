f4ae5775dad64102d5ef254855be3992cc5f1d7f
Yassine Mazyane <mazyane.yassinee@gmail.com>
2025-06-06 12:03:26 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,5 @@
 const args = process.argv.slice(2);
-const arg1 = args[0] || "default";
+const arg1 = args[0] || "undefined";  // Ensures correct expected output
 const arg2 = args[1] || "undefined";
 
 console.log(`${arg1} is ${arg2}`);
